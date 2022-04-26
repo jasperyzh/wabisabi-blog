728ce4d8fe395b52e76ecb2488c654e880b4a464
jasperyzh <jasper.yzh@gmail.com>
2022-04-27 01:07:19 +0800

    reedit entryheader

--- components/EntryHeader.vue
+++ components/EntryHeader.vue
@@ -3,11 +3,8 @@
     <h1>{{ the_content.title }}</h1>
 
     <p class="text-muted m-0">
-      <small>Created at: {{ formatDate(the_content.created_at) }}</small>
-    </p>
-
-    <p class="text-muted m-0">
-      <small>Updated at: {{ formatDate(the_content.updated_at) }}</small>
+      <small>created</small>{{ the_content.created_at }} <small>updated</small
+      >{{ the_content.updated_at }}
     </p>
   </header>
 </template>
